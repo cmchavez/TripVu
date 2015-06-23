92c1d7e9e5e05dd7a254f8a0fc5ebbb16eefb33d
John Ward <ward.johnm@gmail.com>
2015-06-22 17:32:11 -0700

    changes calendar view

--- app/assets/javascripts/daypilot_controller.js
+++ app/assets/javascripts/daypilot_controller.js
@@ -5,10 +5,21 @@ angular
 	      $scope.weekConfig = {
 	          viewType: "Week",
 	      };
-	      $scope.foo = "bar";
-	    console.log("controller works")
-	    };
+	      $scope.daysConfig = {
+	      	  viewType: "Days", 
+	      	  days: 3,
+	      	  businessBeginsHour: 5,
+	      	  businessEndsHour: 20	
 
+	      }
+	      $scope.foo = "bar";
+	    };
+   // .controller('FormCtrl', FormCtrl);
+   //  	function FormCtrl($scope) {
+	  //       $scope.foo="bar"; 
+	  //   	console.log("controller works")
+   //  };   
+	
 //     .config(function($routeProvider, $locationProvider) {$routeProvider
 // 	   .when('/templates/home', {
 // 	    templateUrl: '<%= asset_path("home.html.erb") %>',

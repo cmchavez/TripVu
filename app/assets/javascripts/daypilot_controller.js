093a076631a7e2d5da83d68ca3d849b9474048cb
Cristal Chavez <cristalmchavez@yahoo.com>
2015-06-22 20:22:44 -0700

    includes new itinerary form in trip view

--- app/assets/javascripts/daypilot_controller.js
+++ app/assets/javascripts/daypilot_controller.js
@@ -1,19 +1,47 @@
 angular
 	.module('dayPilotApp.controllers', [])
 	.controller('DayPilotCtrl', DayPilotCtrl);
-		function DayPilotCtrl($scope, $timeout, $http) {
-	      $scope.weekConfig = {
-	          viewType: "Week",
-	      };
-	      $scope.daysConfig = {
-	      	  viewType: "Days", 
-	      	  days: 3,
-	      	  businessBeginsHour: 5,
-	      	  businessEndsHour: 20	
 
-	      };
-	      $scope.foo = "bar";
-	    };
+
+
+
+
+
+// angular.module('formPage', [])
+// 	.controller('ItineraryController', ['$scope', function($scope){
+// 		$scope.master = {};
+
+// 		$scope.update = function(user) {
+// 			$scope.master = angular.copy(user);
+// 		};
+
+// 		$scope.reset = function(form) {
+// 			if (form) {
+// 				form.$setPristine();
+// 				form.$setUntouched();
+// 			}
+// 			$scope.user = angulary.copy($scope.master);
+// 		};
+		
+// 		$scope.reset();
+
+// 	}]);
+
+
+
+		// function DayPilotCtrl($scope, $timeout, $http) {
+	 //      $scope.weekConfig = {
+	 //          viewType: "Week",
+	 //      };
+	 //      $scope.daysConfig = {
+	 //      	  viewType: "Days", 
+	 //      	  days: 3,
+	 //      	  businessBeginsHour: 5,
+	 //      	  businessEndsHour: 20	
+
+	      // };
+	 //      $scope.foo = "bar";
+	    // };
    // .controller('FormCtrl', FormCtrl);
    //  	function FormCtrl($scope) {
 	  //       $scope.foo="bar"; 

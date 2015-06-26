ca5fd57a989bad8ab9aa39b43795b2edaf8f9f5f
Cristal Chavez <cristalmchavez@yahoo.com>
2015-06-25 17:27:57 -0700

    removes create account

--- spec/controllers/user_spec.rb
+++ spec/controllers/user_spec.rb
@@ -1,16 +1,28 @@
 require "rails_helper"
 
 RSpec.describe UsersController do 
+	
+	describe "new action in user controller" do
+		it "creates a user with a name" do
+      	user1 = User.new(first: "john", last: "ward", email: "john@john.com", home:"Santa Monica", age: 27)
+      	expect(user1.first).to eq("john")
+    	end
+  	end
+
 	describe "GET #index" do
 		subject { get :index}
-
 		it "displays the user index page" do
 			expect(subject).to render_template(:index)
 			expect(subject).to render_template("index")
 			expect(subject).to render_template("users/index")
 		end
+
+		it 
 	end
 
-		it ""
+		
+
+
+
 
 end
